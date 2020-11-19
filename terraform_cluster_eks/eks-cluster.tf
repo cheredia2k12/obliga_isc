05e916c1575c3d53d505fa1fa352f3d492f719c8
Christian <cheredia2k12>
2020-11-19 12:11:22 -0300

    subo todo a git

---
+++ terraform_cluster_eks/eks-cluster.tf
@@ -0,0 +1,32 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.18"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "losgalacticos"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 3
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt.id]
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
---
+++ terraform_cluster_eks/local_exec.tf
@@ -0,0 +1,32 @@
+resource "null_resource" "update_kubeconfig" {
+  depends_on = [
+    module.eks.kubeconfig
+  ]
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${local.cluster_name}"
+  }
+  provisioner "local-exec" {
+    command = "kubectl create namespace argocd"
+  }
+  provisioner "local-exec" {
+    command = "kubectl apply -n argocd -f argocd_install.yaml"
+  }
+#  provisioner "local-exec" {
+#    command = "kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'"
+#  }
+ # provisioner "local-exec" {
+ #   command = "curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -"
+ # }
+ # provisioner "local-exec" {
+ #   command = "sudo apt-get install apt-transport-https -y"
+ # }
+ # provisioner "local-exec" {
+ #   command = "echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list"
+ # }
+ # provisioner "local-exec" {
+ #   command = "sudo apt-get update -y && sudo apt-get install helm -y"
+ # }
+ # provisioner "local-exec" {
+ #   command = "helm install --name prometheus stable/prometheus"
+ # }
+}
